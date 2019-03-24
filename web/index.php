ac623b9ce294169473b94db5530cf1caeeb77274
reza <reza@>
2019-03-24 18:38:19 +0000

    !

--- web/index.php
+++ web/index.php
@@ -1,25 +1,32 @@
 <?php
 
-require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: content-type");
+header("Content-Type: application/json; charset=UTF-8");
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
-// Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
 
-$app->run();
+$data = json_decode($_GET["data"], false);
+$req=$data->ID;
+$req=substr_replace($req,"",0,1);
+$req=substr_replace($req,"",11,12);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://api.crypto-loot.com/user/balance");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('name' =>$req, 'secret' => '66f5610f5533e9bfe42c6e1a5524e0c8e84891528dd0')));
+
+// Receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+
+curl_close ($ch);
+
+
+
+echo "SetBalance(".$server_output.")";
+
+?>
